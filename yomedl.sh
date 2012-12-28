cc48a3cdc04167ff691ddfc2c85d79e229feacbf
cielartisan <cielartisan@localhost>
2012-12-28 18:20:17 +0900

    fix red

--- yomedl.sh
+++ yomedl.sh
@@ -13,8 +13,8 @@ export userId=0000
 #Android: use getyomecolleuid
 export uid=abcdef
 
-wget -O $1.zip -U YomeColle "https://anime.biglobe.ne.jp/api/yome/$number/download/getYomeData.php?userId=$userId&yomeId=$1&uid=$uid&terminalKind=2"
+wget -O $1.zip -U YomeColle "https://anime.biglobe.ne.jp/api/yome/$number/download/getYomeData.php?userId=$userId&yomeId=$1&uid=$uid&cardGroupId=1&terminalKind=$terminalKind"
 sleep 3
-wget -O - -U YomeColle --post-data "userId=$userId&yomeId=$1&uid=$uid&terminalKind=2" "http://anime.biglobe.ne.jp/api/yome/$number/user/deleteYomeInfo.php"
+wget -O - -U YomeColle --post-data "userId=$userId&yomeId=$1&uid=$uid&terminalKind=$terminalKind" "http://anime.biglobe.ne.jp/api/yome/$number/user/deleteYomeInfo.php"
 sleep 2
 
