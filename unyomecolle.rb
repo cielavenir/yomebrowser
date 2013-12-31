e43b61e67ec6d3bc26f4e8179fa2fec07140f1db
cielartisan <cielartisan@localhost>
2014-01-01 02:58:57 +0900

    add unyomecolle.rb

---
+++ unyomecolle.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+require 'find'
+require 'fileutils'
+
+if ARGV.size<2
+	puts 'unyomecolle.rb source-dir/ target-dir/'
+	exit
+end
+source=ARGV[0]
+source+='/' if !source.end_with?('/')
+target=ARGV[1]
+target+='/' if !target.end_with?('/')
+Find.find(source){|path|
+	newpath=path.sub(source,target)
+	puts newpath
+	if File.directory?(path)
+		FileUtils.mkdir_p(newpath)
+	elsif path.end_with?('.csv')||path.end_with?('.xml')
+		FileUtils.copy(path,newpath)
+	else
+		File.open(path,'rb'){|f|
+			File.write(newpath,f.each_byte.with_index.map{|e,i|
+					e^(i%0x7f+0x80)
+			}.pack('C*'))
+		}
+	end
+}
--- yomedl.rb
+++ yomedl.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 #YomeBrowser Downloader
 
 #server number
